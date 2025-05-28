ba999d6e5af858dcbec38e3695458824d4111d56
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-28 15:01:56 +0300

    feat(dto): add AuthResponse

---
+++ src/main/java/com/java/planning/dto/AuthResponse.java
@@ -0,0 +1,22 @@
+package com.java.planning.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(description = "Ответ с JWT токеном после успешной аутентификации")
+public class AuthResponse {
+
+    @Schema(
+            description = "JWT токен доступа",
+            example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    private String token;
+}
