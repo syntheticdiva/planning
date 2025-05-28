2df84acf9701421077bec52332e6585963f46b0f
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-28 16:28:45 +0300

    feat(dto): add RegisterRequest

--- src/main/java/com/java/planning/dto/RegisterRequest.java
+++ src/main/java/com/java/planning/dto/RegisterRequest.java
@@ -1,11 +1,12 @@
 package com.java.planning.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
 
 @Data
 @Schema(description = "Запрос на регистрацию пользователя")
