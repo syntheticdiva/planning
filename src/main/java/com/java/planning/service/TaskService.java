9e4dd0484d0a7402caf7ea660f36109b7721e0e7
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-28 15:30:48 +0300

    feat(service): add TaskService

---
+++ src/main/java/com/java/planning/service/TaskService.java
@@ -0,0 +1,36 @@
+package com.java.planning.service;
+
+import com.java.planning.entity.Task;
+import com.java.planning.exception.TaskNotFoundException;
+import com.java.planning.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+    }
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
