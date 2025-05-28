ed8a0158edff1022957e8601b286cedfbee3fb74
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-28 14:59:27 +0300

    feat(entity): add Task

---
+++ src/main/java/com/java/planning/entity/Task.java
@@ -0,0 +1,24 @@
+package com.java.planning.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = true, length = 1000)
+    private String description;
+}
