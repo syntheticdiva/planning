b8dea236c79498448e2c545e6b4fd44df8026ff9
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-29 13:16:10 +0300

    feat(mapper) update TaskMapper

--- src/main/java/com/java/planning/mapper/TaskMapper.java
+++ src/main/java/com/java/planning/mapper/TaskMapper.java
@@ -4,6 +4,7 @@ import com.java.planning.dto.CreateTaskRequest;
 import com.java.planning.dto.TaskDto;
 import com.java.planning.entity.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -12,6 +13,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 public interface TaskMapper {
 
     TaskDto toDto(Task task);
+    @Mapping(target = "id", ignore = true)
     Task toEntity(CreateTaskRequest request);
 
     void updateEntity(@MappingTarget Task task, TaskDto dto);
