552d0e0da4353378d9e7aa308afd6b261e849756
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-29 13:15:04 +0300

    feat(dto) update CreateTaskRequest

--- src/main/java/com/java/planning/dto/CreateTaskRequest.java
+++ src/main/java/com/java/planning/dto/CreateTaskRequest.java
@@ -2,25 +2,28 @@ package com.java.planning.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @Schema(description = "Запрос на создание задачи")
 public class CreateTaskRequest {
 
+    @NotBlank(message = "Title is mandatory")
+    @Size(max = 200)
     @Schema(
             description = "Название задачи",
             example = "Реализовать задачу",
             requiredMode = Schema.RequiredMode.REQUIRED,
-            maxLength = 255
+            maxLength = 200
     )
-    @NotBlank(message = "Title is mandatory")
     private String title;
 
+    @Size(max = 1000)
     @Schema(
             description = "Подробное описание задачи",
-            example = "Необходимо добавить новую фуункцию"
+            example = "Необходимо добавить новую функцию",
+            maxLength = 1000
     )
     private String description;
-
 }
