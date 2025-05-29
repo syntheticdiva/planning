22c5b373dd292f67c328f94bad2f883a80a9ee2f
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-29 13:15:18 +0300

    feat(security) update UserUpdateDto

--- src/main/java/com/java/planning/dto/UserUpdateDto.java
+++ src/main/java/com/java/planning/dto/UserUpdateDto.java
@@ -2,11 +2,13 @@ package com.java.planning.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 @Schema(description = "DTO для обновления данных пользователя")
 public class UserUpdateDto {
     @Schema(description = "Логин пользователя", example = "new_user123")
