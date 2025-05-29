137f95626b32dd2943327cc63123c9d5a39f0b43
synteticdiva <alina.sheveleva2023@mail.ru>
2025-05-29 13:16:50 +0300

    feat(repo) update TaskRepository and UserRepository

--- src/main/java/com/java/planning/repository/TaskRepository.java
+++ src/main/java/com/java/planning/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.java.planning.repository;
 
 import com.java.planning.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
--- src/main/java/com/java/planning/repository/UserRepository.java
+++ src/main/java/com/java/planning/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.java.planning.repository;
 
 import com.java.planning.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByLogin(String login);
